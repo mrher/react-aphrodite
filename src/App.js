86c6decf644635777ea9873b1a477cbcc1225cd1
serg <prog@mailoffice.info>
2018-07-15 16:50:10 +0300

    aprodite css styles

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import {css} from 'aphrodite';
+import {css} from 'aphrodite/no-important';
 import styles from './AppStyles';
 
 class App extends Component {
   render() {
     return (
       <div className={css(styles.container)}>
-        our app
+        <div>our app</div>
+        <div className={css(styles.square)}></div>
       </div>
     );
   }
--- src/AppStyles.js
+++ src/AppStyles.js
@@ -1,7 +1,13 @@
-import { StyleSheet } from 'aphrodite';
+import { StyleSheet } from 'aphrodite/no-important';
+import {blackSquare} from './squareStyles';
 
 export default StyleSheet.create({
         container: {
             background: '#ccc'
+        },
+        square: {
+            ...blackSquare,
+            width: '500px',
+            height: '500px'
         }
 })
